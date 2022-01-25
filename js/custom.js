62092e70e5d6e32b72c4bca627d74e790cb11630
Daniel <84297427+daniox20@users.noreply.github.com>
2022-01-25 12:53:08 -0300

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,3 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
 // owl carousel 
 
 $('.owl-carousel').owlCarousel({
